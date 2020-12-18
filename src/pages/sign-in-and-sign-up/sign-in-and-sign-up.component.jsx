22f036689d08b6fc1bd4a63efaa9a03c3f3788dc
Karolina <kporcioncula.04@gmail.com>
2020-12-18 18:14:43 -0500

    feat: sign in forms

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import SignIn from '../../components/sign-in/sign-in.component'
+export default function SignInAndSignUpPage() {
+  return (
+    <div>
+      <SignIn />
+    </div>
+  )
+}
