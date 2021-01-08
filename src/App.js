97d4d0428b6c3e718adcf0c15ec6058941a3e9df
Karolina <kporcioncula.04@gmail.com>
2021-01-08 18:45:52 -0500

    featL: eset up firebase and firestore

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
-import {auth } from './firebase/firebase.utils'
+import {auth, createUserProfileDocument} from './firebase/firebase.utils'
 import {Switch, Route} from 'react-router-dom'
 import React, { Component } from 'react'
 
@@ -20,8 +20,8 @@ export default class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user=>{
-      this.setState({currentUser: user})
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
+      createUserProfileDocument(user);
     })
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -5,12 +5,37 @@ import 'firebase/auth';
 const config = {
     apiKey: "AIzaSyCukHC3nUY3Vn-H1hkk8YSw7RW3G9Q72hc",
     authDomain: "crwn-clothing-db-a8379.firebaseapp.com",
+    databaseURL: "https://crwn-clothing-db-a8379.firebaseio.com",
     projectId: "crwn-clothing-db-a8379",
     storageBucket: "crwn-clothing-db-a8379.appspot.com",
     messagingSenderId: "94491010909",
     appId: "1:94491010909:web:3a6ebc647bd12b9445eab4",
     measurementId: "G-VN6VN8P45C"
 };
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists){
+        const {displayName, email} = userAuth;
+        const createdAt = new Date()
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message)
+        }
+    }
+    return userRef;
+}
 
 firebase.initializeApp(config);
 
